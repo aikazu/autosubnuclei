d0b2b6e6f8657da5d47ce17f942edd3196baab1f
Iqbal Attila <devas74tor@gmail.com>
2024-01-18 17:33:47 +0700

    update to download latest

--- autosubnuclei.py
+++ autosubnuclei.py
@@ -3,57 +3,60 @@ import subprocess
 import sys
 import os
 import shutil
-import urllib.request
+import requests
 import zipfile
 import tempfile
 
-def run_command(command):
+GITHUB_API_URL = "https://api.github.com/repos/projectdiscovery/{binary}/releases/latest"
+
+def get_amd64_zip_url(release_info):
+    assets = release_info.get("assets", [])
+    for asset in assets:
+        if "amd64" in asset["name"].lower() and asset["name"].endswith(".zip"):
+            return asset["browser_download_url"]
+
+    sys.exit(1)
+
+def get_latest_release_url(binary):
+    url = GITHUB_API_URL.format(binary=binary)
+    response = requests.get(url)
+    if response.status_code != 200:
+        sys.exit(1)
+
+    release_info = response.json()
+    return get_amd64_zip_url(release_info)
+
+def run_command(command, step_name):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
 
     if process.returncode != 0:
-        print(f"Error: {error.decode('utf-8')}")
+        print(f"Error at step '{step_name}': {error.decode('utf-8')}")
         sys.exit(1)
 
     return output.decode('utf-8')
 
 def download_and_extract(url, binary_name):
     print(f"{binary_name} not found, downloading...")
-    file_name = url.split("/")[-1]
-    urllib.request.urlretrieve(url, file_name)
 
-    print(f"Extracting {binary_name}...")
+    response = requests.get(url, stream=True)
     temp_dir = tempfile.mkdtemp()
-    with zipfile.ZipFile(file_name, 'r') as zip_ref:
-        members = [member for member in zip_ref.namelist() if member.endswith(binary_name)]
-        if not members:
-            print(f"Error: {binary_name} not found in the zip file.")
-            sys.exit(1)
+    zip_file_path = os.path.join(temp_dir, binary_name + ".zip")
 
-        zip_ref.extractall(path=temp_dir)
+    with open(zip_file_path, "wb") as zip_file:
+        for chunk in response.iter_content(chunk_size=128):
+            zip_file.write(chunk)
 
-    bin_directory = "/bin/"
-    binary_path = os.path.join(temp_dir, members[0])
-
-    if not os.path.exists(binary_path):
-        print(f"Error: {binary_name} not found after extraction.")
+    try:
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            zip_ref.extractall(path=temp_dir)
+    except zipfile.BadZipFile:
+        print(f"Error: {binary_name} download failed. The file is not a valid zip file.")
+        shutil.rmtree(temp_dir)
         sys.exit(1)
 
-    if not os.access(bin_directory, os.W_OK):
-        if os.geteuid() != 0:
-            print(f"Please run the script with sudo to move {binary_name} to {bin_directory}")
-            sys.exit(1)
-        else:
-            print(f"Warning: {bin_directory} is not writable, attempting to move with sudo...")
+    shutil.move(os.path.join(temp_dir, binary_name), os.path.dirname(os.path.realpath(__file__)))
 
-    # Mark the binary as executable
-    os.chmod(binary_path, 0o755)
-
-    print(f"Moving {binary_name} to {bin_directory}...")
-    shutil.move(binary_path, os.path.join(bin_directory, binary_name))
-
-    # Clean up: remove the downloaded zip file and temporary folder after extraction
-    os.remove(file_name)
     shutil.rmtree(temp_dir)
 
 def main():
@@ -64,9 +67,9 @@ def main():
     domain = sys.argv[1]
 
     binaries = {
-        "subfinder": "https://github.com/projectdiscovery/subfinder/releases/download/v2.6.4/subfinder_2.6.4_linux_amd64.zip",
-        "httpx": "https://github.com/projectdiscovery/httpx/releases/download/v1.3.8/httpx_1.3.8_linux_amd64.zip",
-        "nuclei": "https://github.com/projectdiscovery/nuclei/releases/download/v3.1.5/nuclei_3.1.5_linux_amd64.zip"
+        "subfinder": get_latest_release_url("subfinder"),
+        "httpx": get_latest_release_url("httpx"),
+        "nuclei": get_latest_release_url("nuclei")
     }
 
     for binary, url in binaries.items():
@@ -75,15 +78,15 @@ def main():
 
     # Use Subfinder to find subdomains and save them to a file
     print("Finding subdomains with Subfinder...")
-    run_command(f"subfinder -silent -all -d {domain} -o {domain}_subfinder")
+    run_command(f"subfinder -silent -all -d {domain} -o {domain}_subfinder", "Subfinder")
 
     # Use Httpx to find live subdomains and save them to a file
     print("Finding live subdomains with Httpx...")
-    run_command(f"httpx -silent -l {domain}_subfinder -o {domain}_httpx")
+    run_command(f"httpx -silent -l {domain}_subfinder -o {domain}_httpx", "Httpx")
 
     # Use Nuclei to scan the live subdomains with a specific template
     print("Scanning live subdomains with Nuclei...")
-    run_command(f"nuclei -l {domain}_httpx -t ~/nuclei-templates/ -me {domain}_nuclei")
+    run_command(f"nuclei -l {domain}_httpx -t ~/nuclei-templates/ -me {domain}_nuclei", "Nuclei")
 
     print("Done!")
 
